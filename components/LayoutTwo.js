582e4dc1761bf148c1f2c092dc7e4a1864560d2c
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-04-22 19:57:10 -0400

    Update LayoutTwo.js

--- components/LayoutTwo.js
+++ components/LayoutTwo.js
@@ -8,9 +8,6 @@ export default function LayoutTwo({ title, children, keywords, description }) {
                 <title>{title}</title>
                 <meta name="keywords" content={keywords} />
                 <meta name="description" content={description} />
-                <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
-                <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
-                <link href="/css/blog.css" rel="stylesheet" type="text/css" />
             </Head>
             <main>
                 {children}
