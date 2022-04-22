1a9aadd6e5bfc422554ba00397ff530482a07d43
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-04-22 19:40:34 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,6 @@ export default function Layout({ title, children, keywords, description }) {
                 <title>{title}</title>
                 <meta name="keywords" content={keywords} />
                 <meta name="description" content={description} />
-                <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
                 <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
                 <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
                 <link href="/css/globals.css" rel="stylesheet" type="text/css" />
