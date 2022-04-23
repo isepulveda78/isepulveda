ee99324d29dd3cee1b0c3e0f3347e1040502506e
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-04-22 20:30:31 -0400

    Update Categories.js

--- components/Categories.js
+++ components/Categories.js
@@ -3,8 +3,10 @@ import Link from "next/link"
 export default function Categories({categories}){
     return (
                 <>
-                        {categories.map((category, index) => (
-                            <Link key={index} href={`/blog/category/${category.toLowerCase()}`}><li>{category}</li></Link>
+                        {categories.map((category) => (
+                            <Link href={`/blog/category/${category.toLowerCase()}`}>
+                                <li>{category}</li>
+                            </Link>
                         ))}
                 </>
     )
