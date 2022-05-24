a1ea4b50b327b95faba9c147db1a3c551e32e779
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-05-24 16:30:36 -0400

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,7 +3,7 @@ export default function Navigation(){
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-padding">
         <div className="container">
-            <Link href="/"><a className="navbar-brand">isepulveda</a></Link>
+            <Link href="/"><div className="navbar-brand">isepulveda</div></Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <div className="ms-auto">
