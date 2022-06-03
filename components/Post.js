18120bf9696ef70587a9097f0054324ecf9de181
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-06-03 11:03:25 -0400

    rounded-bottom

--- components/Post.js
+++ components/Post.js
@@ -5,7 +5,7 @@ export default function Post({post, cats}){
         <div className="col-lg-6 my-2 portfolio">
             <div className="card h-100">
             <Image src={post.thumbnail} width={1366} height={625} alt={post.name} className="card-img-top img-fluid" />
-                <div className="card-body bg-white text-dark rounded">
+                <div className="card-body bg-white text-dark rounded-bottom">
                     <div className="card-title text-center"><Link href={post.acf.website}>{post.title.rendered}</Link></div>
                     <hr/>
                     {cats}
