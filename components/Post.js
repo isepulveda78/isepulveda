117963d2facd6ed77ff64fb948c805f83798a378
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-06-03 11:00:22 -0400

    card color change

--- components/Post.js
+++ components/Post.js
@@ -2,11 +2,11 @@ import Link from "next/link"
 import Image from "next/image"
 export default function Post({post, cats}){
     return (
-        <div className="col-lg-6 my-2">
+        <div className="col-lg-6 my-2 portfolio">
             <div className="card h-100">
             <Image src={post.thumbnail} width={1366} height={625} alt={post.name} className="card-img-top img-fluid" />
-                <div className="card-body">
-                    <h2 className="card-title h4 text-center"><Link href={post.acf.website}>{post.title.rendered}</Link></h2>
+                <div className="card-body bg-white text-dark rounded">
+                    <div className="card-title text-center"><Link href={post.acf.website}>{post.title.rendered}</Link></div>
                     <hr/>
                     {cats}
                 </div>
