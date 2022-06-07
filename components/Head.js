96ac0f6e6c38e185c44294abc4cec9ce2e5ef13d
Israel Sepulveda <sepulveda.israel@gmail.com>
2022-06-07 09:03:25 -0400

    added className

--- components/Head.js
+++ components/Head.js
@@ -4,8 +4,8 @@ export default function Head(){
         <header className="py-5 border-bottom mb-4 background-header">
                 <div className="text-center my-5 text-dark">
                     <h2 className="font-pacifico">Welcome</h2>
-                <Link href={'#portfolio'}><a type="button" class="btn btn-outline-dark btn-lg me-1">Portfolio</a></Link>
-                <Link href={'/blog'}><a type="button" class="btn btn-outline-dark btn-lg">Blog</a></Link>
+                <Link href={'#portfolio'}><a type="button" className="btn btn-outline-dark btn-lg me-1">Portfolio</a></Link>
+                <Link href={'/blog'}><a type="button" className="btn btn-outline-dark btn-lg">Blog</a></Link>
                 </div>
         </header>
     )
